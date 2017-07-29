82fb4f58492384c8e643b04ddee799fd448f8a25
Eirik Horvath <eirik@geta.no>
2017-07-29 21:18:10 +0200

    added more tests

--- test/data-store.spec.js
+++ test/data-store.spec.js
@@ -8,14 +8,36 @@ describe('given an instance of DataStore', () => {
         ds = new DataStore();
         number = Math.random();
     });
-    describe('when I supply the name', () => {
+    describe('when I supply the data', () => {
         before(() => {
             ds.add({
                 testData: number,
             });
         })
-        it('should return the same number', () => {
+        it('should return the same data', () => {
             expect(ds.get().testData).to.be.equal(number);
         })
     })
 });
+
+describe('given multiple instances of DataStore', () => {
+    let ds1;
+    let number;
+        before(() => {
+        ds1 = new DataStore();
+        number = Math.random();
+    });
+    describe('when I supply the data', () => {
+        let ds2;
+        before(() => {
+            ds1.add({
+                testData: number,
+            });
+
+            ds2 = new DataStore();
+        });
+        it('the data should be the same', () => {
+            expect(ds1.get().testData).to.be.equal(ds2.get().testData);
+        })
+    })
+})
